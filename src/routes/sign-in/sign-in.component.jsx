ea125a7edf1f4d288086c2681b076e8ff42b65d1
unknown <selim.lipovica@outlook.com>
2022-09-27 15:56:16 +0200

    created sign up component, the form input component, button component

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,6 +1,14 @@
-import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
 
-import { createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import {
+  auth,
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
 const SignIn = () => {
   const logGoogleUser = async () => {
@@ -12,6 +20,7 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google PopUp</button>
+      <SignUpForm />
     </div>
   );
 };
